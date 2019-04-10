433b47f7cadbb9e8c7dc2cb01c3bd532e108c7a4
yknoaki <yknoaki@gmail.com>
2019-04-10 15:24:08 +0900

    for my practice

---
+++ Untitled-1.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	flag.Parse()
+	var args []string = flag.Args()
+	
+
+}
+
+func sort(str string)ary []int{
+	ary = strings.Split(str, "")
+	fmt.Println(ary)
+}
