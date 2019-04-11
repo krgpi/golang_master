8c2cbd9afdb2a7eb10590d6ccc24be86c6eca584
Yuki NOAKI <yuki@yukis-MBP-7.local>
2019-04-11 10:39:16 +0900

    コメント追加

--- sort.go
+++ sort.go
@@ -7,19 +7,22 @@ import (
 	"strings"
 )
 
+//sort バブルソート
 func sort(str string) (ary []string) {
+	//1文字ずつ分割して配列にする
 	ary = strings.Split(str, "")
+	//ソート開始
 	for index := range ary {
 		if index > 0 {
 			for i := index; ; i = i - 1 {
 				if i > 0 {
-					cur, _ := strconv.Atoi(ary[i])
-					bef, _ := strconv.Atoi(ary[i-1])
-					if bef > cur {
+					cur, _ := strconv.Atoi(ary[i])   //current
+					bef, _ := strconv.Atoi(ary[i-1]) //before
+					if bef > cur {                   //小さい順でなかったら入れ替える
 						save := ary[i-1]
 						ary[i-1] = ary[i]
 						ary[i] = save
-					} else {
+					} else { //小さい順になってたら何もしない
 						break
 					}
 				} else {
@@ -32,6 +35,7 @@ func sort(str string) (ary []string) {
 }
 
 func main() {
+	//コマンドライン引数を取得
 	flag.Parse()
 	var args []string = flag.Args()
 	fmt.Println(sort(args[0]))
